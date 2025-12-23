4b26740624280dc453d0c2431313ec8b40864211
数智未来DLCN <dlcncc@foxmail.com>
2025-12-23 21:17:54 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,6 +11,19 @@ export default defineUserConfig({
 
   theme,
 
+  // 全局head配置，添加百度统计代码
+  head: [
+    ["script", {}, `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?05d159d4e872167c81e1828a0171ae31";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();
+    `]
+  ],
+
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
